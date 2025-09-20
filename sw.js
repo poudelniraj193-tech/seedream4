a3e0e43a7904f32b9a7ba8b39c0c4691a4c0b751
poudelniraj193-tech <poudelniraj193@gmail.com>
2025-09-20 03:55:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("seedream-cache").then(cache => {
+    caches.open("seedream-cache").then((cache) => {
       return cache.addAll([
         "/",
         "/index.html",
@@ -12,8 +12,10 @@ self.addEventListener("install", e => {
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
