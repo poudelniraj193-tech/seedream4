bd5cc424ebd9a383314e9d0ae5a11caf09771250
poudelniraj193-tech <poudelniraj193@gmail.com>
2025-09-20 04:01:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches.open("seedream-cache").then((cache) => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
+        "index.html",
+        "manifest.json",
+        "Logo.jpg",
+        "icons/icon-192.png",
+        "icons/icon-512.png"
       ]);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
